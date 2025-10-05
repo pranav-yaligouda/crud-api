f77f4906df2ba046be27de1ae6e4bf0fc10e8cbd
pranav-yaligouda <pranavyaligouda7@gmail.com>
2025-10-05 17:47:50 +0530

    Created user model with name email phone gender dob password fields

---
+++ src/models/user.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const userScheme = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Name is required"],
+      trim: true,
+      minLength: 3,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [/\S+@\S+\.\S+/, "Please fill valid email address"],
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ["Male", "Female", "Other"],
+    },
+    dob: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return value <= new Date();
+        },
+        message: "Date of birth should not be in the future",
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
